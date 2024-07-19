2dbb79c2ed9c169a4099e38fc8fafc55f15c8ece
Kristina <kristinashrestha2055@gmail.com>
2024-07-18 20:00:15 -0400

    bubble sorting test

--- main_test.go
+++ main_test.go
@@ -4,21 +4,27 @@ import (
 	"testing"
 )
 
-func TestStringLength(t *testing.T) {
+func TestBubbleSort(t *testing.T) {
 	tests := []struct {
-		input    string
-		expected int
+		arr      []int
+		expected []int
 	}{
-		{"Kristina", 8},
-		{"shrestha", 8},
-		{"Data", 4},
-		{"", 0}, // Edge case: empty string
+		{[]int{64, 34, 25, 12, 22, 11, 90}, []int{11, 12, 22, 25, 34, 64, 90}},
+		{[]int{5, 1, 4, 2, 8}, []int{1, 2, 4, 5, 8}},
+		{[]int{3, 0, 2, 5, -1, 4, 1}, []int{-1, 0, 1, 2, 3, 4, 5}},
+		{[]int{}, []int{}}, 
+		{[]int{1}, []int{1}}, 
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}}, 
+		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}}, 
 	}
 
 	for _, test := range tests {
-		result := StringLength(test.input)
-		if result != test.expected {
-			t.Errorf("StringLength(%q) = %d; want %d", test.input, result, test.expected)
+		result := BubbleSort(test.arr)
+		for i := range result {
+			if result[i] != test.expected[i] {
+				t.Errorf("BubbleSort(%v) = %v; want %v", test.arr, result, test.expected)
+				break
+			}
 		}
 	}
 }
